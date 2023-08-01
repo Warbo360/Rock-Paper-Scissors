0766b51df1e508f8fcaef81ed17954d557bb174b
Warbo360 <wbetz360@gmail.com>
2023-08-01 02:35:08 -0500

    Generated the function for the computer to randomly generate an option in the console.
    There are no parameter to the function itself, once function is called, random number is generated and the function carrys out a if else where the conditions are dependent on the random number generated, since there are three options each option was given a 1/3 section of 0 to 1 assigned to it to make the function return a value of the form of a string from the choices: rock, paper, scissors. Next step would be assiging a variable (computerChoice) that is equal to the return value of this function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,11 +1,29 @@
-// prompt user to begin game/round with enter click
+// prompt user to begin game/round with enter click (for later!)
+// For now will keep in browser as a prompt to being to start game
 
 // computer rando picks one of the three options
+
+function getComputerChoice() {
+    let result = Math.random();
+    console.log(result);
+    if (result <= 0.33) {
+        result = "rock";
+        console.log(result);
+    } else if (result >0.33 && result <= 0.66) {
+        result = "paper";
+        console.log(result);
+    } else {
+        result = "scissors";
+        console.log(result);
+    }
+    return result;
+}
+    
 
 // user is prompted to type (make it not case sensitive) one of the three options
 
 // program compares the two chosen values made by computer and user and a winner is chosen (implied this is where we tell the program conditions of victory [rock beats scissor, scissors beats paper, paper beats rock], no points awarded for a tie).
-
+      
 // user is met with a statement of winning or losing the round
 
 // running total of the series updated
